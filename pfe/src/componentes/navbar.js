e58dc934aaea10a7f8194c96de480749e767b85b
Antonio Salinas R <antonio.salinasr@gmail.com>
2022-09-12 20:03:04 -0300

    clase5

--- pfe/src/componentes/navbar.js
+++ pfe/src/componentes/navbar.js
@@ -1,7 +1,6 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from "react-router-dom";
 
 function NavBarCustom() {
@@ -9,24 +8,13 @@ function NavBarCustom() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand><Link to="/" style={linkStyle}>Programacion FrontEnd</Link></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link><Link to="/login" style={linkStyle}>Login</Link></Nav.Link>
-            <Nav.Link><Link to="/about" style={linkStyle}>About</Link></Nav.Link>
-            <Nav.Link><Link to="/info" style={linkStyle}>Info</Link></Nav.Link>
-            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown> */}
+            <Nav.Link><Link to="/register" style={linkStyle}>Register</Link></Nav.Link>
+            <Nav.Link><Link to="/dashboard" style={linkStyle}>Dashboard</Link></Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
